9478d0e3349d9fc4de4644455eca685a8a132089
Juan Allin Cañas <juan.allin@sofka.local>
2022-07-19 12:35:00 -0500

    Initial commit with dependencies

---
+++ src/main/java/com/ejercicio/zonadetalentos/EjercicioBackEndZonaTalentosSofkaApplication.java
@@ -0,0 +1,13 @@
+package com.ejercicio.zonadetalentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioBackEndZonaTalentosSofkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioBackEndZonaTalentosSofkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejercicio/zonadetalentos/EjercicioBackEndZonaTalentosSofkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicio.zonadetalentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioBackEndZonaTalentosSofkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
